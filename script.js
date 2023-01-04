82cf826e98af8bd3030b4fc2723685290022c68a
kevinld93 <121909296+kevinld93@users.noreply.github.com>
2023-01-04 10:22:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-  var x=document.getElementById("trabajo");
+   var x=document.getElementById("trabajo");
 
   x.onclick=trabajo;
   
   function trabajo(){
-   document.getElementById("t").innerHTML="Desarrollo Front end y back end de aplicaciones con typescript,Javascript,PHP,MySQL,HTML,Realización de test de aplicaciones,Solución de incidencias "
+   document.getElementById("t").innerHTML="*Programador full stack en Finanbest,Madrid:Desarrollo Front end y back end de aplicaciones con typescript,Javascript,PHP,MySQL,HTML,Realización de test de aplicaciones,Solución de incidencias <br> *Programadoren en Servicios técnicos it,bilbao:toma de requerimientos con el cliente,análisis de requerimientos y diseño de aplicaciones,desarrolo de aplicaciones utilizando javascript,PHP,MySQL,soporte tecnico a clientes"
 
   
    document.getElementById("titulo").innerHTML="Experiencia"
@@ -15,7 +15,7 @@
   i.onclick=sobre;
   
   function sobre(){
-   document.getElementById("t").innerHTML="Técnico superior en desarrollo de aplicaciones y plataformas con amplios conocimientos en back-end, desarrollados a través de proyectos propios.Busco desarrollar mi carrera, ayudando a TechSoft Solutions, contribuyendo con mis habilidades en C#, ASP.NET y Azure para lograr diseños finales que satisfagan las necesidades y preferencias de los clientes."
+   document.getElementById("t").innerHTML="Técnico superior en desarrollo de aplicaciones y plataformas con amplios conocimientos en back-end, desarrollados a través de proyectos propios. <br> Busco desarrollar mi carrera, ayudando a TechSoft Solutions, contribuyendo con mis habilidades en C#, ASP.NET y Azure para lograr diseños finales que satisfagan las necesidades y preferencias de los clientes."
    document.getElementById("titulo").innerHTML="Sobre mi"}
 
    var f=document.getElementById("Formacion");
@@ -23,5 +23,5 @@
   f.onclick=Formacion;
   
   function Formacion(){
-   document.getElementById("t").innerHTML=" Ingenieria de telecomunicaciones:Universidad de Navarra"
+   document.getElementById("t").innerHTML=" *Grado en ingeniería informática universidad autónoma de Barcelona,España septiembre de 2012-Julio de 2017  <br>     *Bachillerato instituto Poeta Maragall,Septiembre de 2010-Julio de 2012"
    document.getElementById("titulo").innerHTML="Formación"}
